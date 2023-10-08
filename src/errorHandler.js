bf4b74eb56d9a3f19afd7aeb90c8fa81b5057d5e
axel-ac <abdullahceylan.axel@gmail.com>
2023-10-08 17:18:20 +0200

    blogController

--- src/errorHandler.js
+++ src/errorHandler.js
@@ -5,7 +5,7 @@
 
 module.exports = (err, req, res, next) => {
 
-    const errorStatusCode = res.errorStatusCode ?? 500;
+    const errorStatusCode = res?.errorStatusCode ?? 500;
     res.status(errorStatusCode).send({
         error: true,
         message: err.message,
