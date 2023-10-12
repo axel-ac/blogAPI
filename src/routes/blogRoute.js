bcb606b8f8297c2cc994e5c4cbb99383d8ffd251
axel-ac <abdullahceylan.axel@gmail.com>
2023-10-12 20:36:44 +0200

    .

--- src/routes/blogRoute.js
+++ src/routes/blogRoute.js
@@ -33,6 +33,6 @@ router.route('/post/:postId')
     .put(BlogPost.update)
     .delete(BlogPost.delete)
 
-// router.get('/category/:categoryId/posts', BlogPost.listCategoryPosts)
+router.get('/category/:categoryId/posts', BlogPost.listCategoryPosts)
 
 module.exports = router
