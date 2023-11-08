3c69808021bdbc04a7070c66d1f92eab32990b39
axel-ac <abdullahceylan.axel@gmail.com>
2023-11-08 18:45:03 +0100

    index

--- index.js
+++ index.js
@@ -8,27 +8,49 @@
  * $ npm i mongoose
  */
 
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
-require("dotenv").config()
+require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 
-app.use(express.json())
+/* ------------------------------------------------------- */
+// SessionCookies:
+// http://expressjs.com/en/resources/middleware/cookie-session.html
+// https://www.npmjs.com/package/cookie-session
+//* $ npm i cookie-session
+const session = require("cookie-session");
+app.use(
+  session({ secret: process.env.SECRET_KEY || "secret_keys_for_cookies" })
+);
+/* ------------------------------------------------------- */
+// Template:
+// npm i ejs
+
+/* ------------------------------------------------------- */
+// Accept json data & convert to object:
+app.use(express.json());
 
 // Connect to MongoDB with Mongoose:
-require('./src/dbConnection')
+require("./src/dbConnection");
+
+// Searching&Sorting&Pagination:
+app.use(require("./src/middlewares/findSearchSortPage"));
 
 // HomePage:
 app.all("/", (req, res) => {
-    res.send("WELCOME TO BLOG API");
-})
+  res.send("WELCOME TO BLOG API");
+});
+
+// Routes:
+app.use("/user", require("./src/routes/userRoute"));
+app.use("/blog", require("./src/routes/blogRoute"));
 
 /* ------------------------------------------------------- */
 // Synchronization:
 // require('./src/sync')()
 
-//errorHandler:
-app.use(require("./src/errorHandler"))
+// errorHandler:
+app.use(require("./src/errorHandler"));
 
 app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
